dd10e908424ecd0546fc16bdce812d35b90af380
Angie Hong <lsuvhyc1@gmail.com>
2024-05-16 13:58:48 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1>Angie Hong</h1>
+      <h2>Software Developer</h2>
+    
+    </>
+  )
+}
+
+export default App;
+//npm run dev means run in developer environment
